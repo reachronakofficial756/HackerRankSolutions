96cbc95dc9ae985c930411f55da6b55dd5b68afe
Ronak Jain <reachronakofficial@gmail.com>
2025-02-10 13:52:56 +0530

    Added a New steel Gradation Classifier

---
+++ Steel.Gradation.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+  int h,t;
+    float c;
+    scanf("%d %f %d",&h,&c,&t);
+    
+    if(h>50 && c<0.7 && t>5600){
+        printf("The grade of the steel is: 10\nAll of the conditions met.");
+    }
+    else if(h>50 && c<0.7){
+       printf("The grade of the steel is: 9\nTwo conditions met."); 
+    }
+    else if(c<0.7 && t>5600){
+        printf("The grade of the steel is: 8\nTwo conditions met.");
+    }
+    else if(t>5600 && h>50){
+        printf("The grade of the steel is: 7\nTwo conditions met.");
+    }
+    else if(h>50 || c<0.7 || t>5600){
+        printf("The grade of the steel is: 6\nOnly one condition met.");
+    }
+    else{
+        printf("The grade of the steel is: 5\nNone of the conditions met.");
+    }
+    
+    return 0; 
+}
